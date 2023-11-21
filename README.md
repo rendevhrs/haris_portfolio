98b8d1db422f50379b42f4a32360a973d0a3335b
rendev <harisnf300@gmail.com>
2023-11-21 18:36:23 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# harisnf
+ Portfolio Haris
---
+++ src/components/resume/Resume.jsx
@@ -0,0 +1,230 @@
+import React from "react";
+
+const Resume = () => {
+  return (
+    <div id="resume" className=" container m-auto   mt-16">
+      {/* heading */}
+      <div data-aos="fade-up" className="relative mb-5">
+        <h3 className=" text-3xl font-black text-gray-400 sm:text-2xl">
+          Resume
+        </h3>
+        <span className="h-[1.1px] right-0 absolute w-[92%] bg-gray-300 block"></span>
+      </div>
+      <div data-aos="fade-up" className="left flex-1 w-full">
+        <p className=" text-gray-700 font-medium w-[100%]">
+          Here are my experiences and qualifications.
+        </p>
+      </div>
+      {/* card*/}
+      <div className="card-wrapper w-[90%] sm:w-full mt-5 flex md:flex-col sm:gap-5 mx-auto ">
+        <div className="left flex-1 flex items-center justify-center">
+          <fieldset
+            data-aos="zoom-in"
+            className=" w-[80%] p-5 py-12 sm:py-8 sm:w-full sm:p-2"
+          >
+            <legend className=" w-auto ml-[50%] translate-x-[-50%] border-2 border-gray-200 rounded-3xl py-1 px-8 font-semibold text-xl text-violet-500">
+              Experience
+            </legend>
+            <div className=" relative">
+              {/* design */}
+              <div className="design flex absolute left-[-150px] top-1/2 items-center rotate-[90deg] sm:left-[-160px] ">
+                <div className="c1 w-[12px] h-[12px] rounded-full bg-white border-2 border-violet-500"></div>
+                <div className="line w-[230px] bg-gray-300 h-[2px] sm:w-[250px]"></div>
+                <div className="c2 w-[12px] h-[12px] rounded-full bg-white border-2 border-violet-500"></div>
+              </div>
+              {/* design */}
+              <div className=" flex flex-col gap-1 sm:gap-1 border-2 border-violet-400 shadow-[0px_0px_16px_1px_rgba(0,0,0,0.1)] p-3 rounded-lg">
+                <h1 className="text-[1.4rem] font-semibold sm:text-xl">
+                  Analyst
+                </h1>
+                <span className=" text-[.9rem] font-semibold text-gray-500 sm:text-base">
+                  GudangKripto
+                </span>
+                <span className=" text-[.9rem] font-semibold text-violet-500 sm:text-base">
+                  Feb 2022 - Present
+                </span>
+                <p className=" text-[.9rem] text-justify break-words text-gray-500">
+                  - Identifying and analyzing business requirements and formulating appropriate solutions. <br />
+                  - Designing technical plans, including system architecture, user interfaces, and integration with other systems. <br />
+                  - Collaborating with the development team, ensuring a clear understanding of the application flow, and
+                  overseeing progress. <br />
+                  - Developing test scenarios, overseeing application testing, identifying application bugs, and ensuring
+                  smooth functionality. <br />
+                  - Provide training to users on the use of GudangKripto website and mobile app. <br />
+                  - Handling post-implementation issues and enhancing systems according to needs. <br />
+                  - Frequently used tools include Jira, Trello, Gmail, Excel, Word, Drawio, etc. 
+                </p>
+              </div>
+            </div>
+          </fieldset>
+        </div>
+
+        <div className="left flex-1 flex items-center justify-center">
+          <fieldset
+            data-aos="zoom-in"
+            className=" w-[80%] p-5 py-12 sm:py-8 sm:w-full sm:p-2"
+          >
+            <legend className=" w-auto ml-[50%] translate-x-[-50%] border-2 border-gray-200 rounded-3xl py-1 px-8 font-semibold text-xl text-violet-500">
+              Experience
+            </legend>
+            <div className=" relative">
+              {/* design */}
+              <div className="design flex absolute left-[-150px] top-1/2 items-center rotate-[90deg] sm:left-[-160px] ">
+                <div className="c1 w-[12px] h-[12px] rounded-full bg-white border-2 border-violet-500"></div>
+                <div className="line w-[230px] bg-gray-300 h-[2px] sm:w-[250px]"></div>
+                <div className="c2 w-[12px] h-[12px] rounded-full bg-white border-2 border-violet-500"></div>
+              </div>
+              {/* design */}
+              <div className=" flex flex-col gap-1 sm:gap-1 border-2 border-violet-400 shadow-[0px_0px_16px_1px_rgba(0,0,0,0.1)] p-3 rounded-lg">
+                <h1 className="text-[1.4rem] font-semibold sm:text-xl">
+                  Quality Assurance Specialist
+                </h1>
+                <span className=" text-[.9rem] font-semibold text-gray-500 sm:text-base">
+                  GudangKripto
+                </span>
+                <span className=" text-[.9rem] font-semibold text-violet-500 sm:text-base">
+                  Feb 2022 - Present
+                </span>
+                <p className=" text-[.9rem] text-justify break-words text-gray-500">
+                  - Conduct comprehensive quality testing on the GudangKripto application. <br />
+                  - Develop a comprehensive testing plan, test cases, and test scripts to ensure extensive coverage of
+                  quality evaluation. <br />
+                  - Identify and document bugs or discrepancies in the GudangKripto application and report findings to
+                  the relevant team. <br />
+                  - Collaborate with developers, designers, and product managers to provide clear explanations of the identified issues. <br />
+                  - Analyze testing processes and methodologies, seeking opportunities for refinement and efficiency enhancement. <br />
+                  - Perform regular regression testing to ensure that software updates, enhancements, or modifications
+                  do not adversely affect existing functionality. <br />
+                  - Ensure that products or services adhere to established quality standards, industry best practices, and regulatory requirements. <br />
+                </p>
+              </div>
+            </div>
+          </fieldset>
+        </div>
+
+        <div className="left flex-1 flex items-center justify-center">
+          <fieldset
+            data-aos="zoom-in"
+            className=" w-[80%] p-5 py-12 sm:py-8 sm:w-full sm:p-2"
+          >
+            <legend className=" w-auto ml-[50%] translate-x-[-50%] border-2 border-gray-200 rounded-3xl py-1 px-8 font-semibold text-xl text-violet-500">
+              Experience
+            </legend>
+            <div className=" relative">
+              {/* design */}
+              <div className="design flex absolute left-[-150px] top-1/2 items-center rotate-[90deg] sm:left-[-160px] ">
+                <div className="c1 w-[12px] h-[12px] rounded-full bg-white border-2 border-violet-500"></div>
+                <div className="line w-[230px] bg-gray-300 h-[2px] sm:w-[250px]"></div>
+                <div className="c2 w-[12px] h-[12px] rounded-full bg-white border-2 border-violet-500"></div>
+              </div>
+              {/* design */}
+              <div className=" flex flex-col gap-1 sm:gap-1 border-2 border-violet-400 shadow-[0px_0px_16px_1px_rgba(0,0,0,0.1)] p-3 rounded-lg">
+                <h1 className="text-[1.4rem] font-semibold sm:text-xl">
+                  Staff System Analyst
+                </h1>
+                <span className=" text-[.9rem] font-semibold text-gray-500 sm:text-base">
+                  PT Barifal Mega Fortuna
+                </span>
+                <span className=" text-[.9rem] font-semibold text-violet-500 sm:text-base">
+                  Oct 2020 - Feb 2022 (1 year 5 months)
+                </span>
+                <p className=" text-[.9rem] text-justify break-words text-gray-500">
+                  - Utilizing creativity to propose innovative approaches for process enhancement and problem-solving.<br />
+                  - Leveraging problem-solving skills to address system issues and streamline workflows.<br />
+                  - Developing comprehensive software documentation, including technical specifications and user
+                  guides.<br />
+                  - Acting as a bridge between technical and non-technical teams, facilitating effective communication.<br />
+                  - Participating in project planning and execution, ensuring alignment with organizational objectives.<br />
+                  - Conducting training sessions to empower staff with an understanding of systems and optimal
+                  application usage.<br />
+                  - Continuously improving processes by applying insights gained from data analysis.
+                </p>
+              </div>
+            </div>
+          </fieldset>
+        </div>
+
+        <div className="left flex-1 flex items-center justify-center">
+          <fieldset
+            data-aos="zoom-in"
+            className=" w-[80%] p-5 py-12 sm:py-8 sm:w-full sm:p-2"
+          >
+            <legend className=" w-auto ml-[50%] translate-x-[-50%] border-2 border-gray-200 rounded-3xl py-1 px-8 font-semibold text-xl text-violet-500">
+              Experience
+            </legend>
+            <div className=" relative">
+              {/* design */}
+              <div className="design flex absolute left-[-150px] top-1/2 items-center rotate-[90deg] sm:left-[-160px] ">
+                <div className="c1 w-[12px] h-[12px] rounded-full bg-white border-2 border-violet-500"></div>
+                <div className="line w-[230px] bg-gray-300 h-[2px] sm:w-[250px]"></div>
+                <div className="c2 w-[12px] h-[12px] rounded-full bg-white border-2 border-violet-500"></div>
+              </div>
+              {/* design */}
+              <div className=" flex flex-col gap-1 sm:gap-1 border-2 border-violet-400 shadow-[0px_0px_16px_1px_rgba(0,0,0,0.1)] p-3 rounded-lg">
+                <h1 className="text-[1.4rem] font-semibold sm:text-xl">
+                  Quality Assurance Specialist
+                </h1>
+                <span className=" text-[.9rem] font-semibold text-gray-500 sm:text-base">
+                  PT Barifal Mega Fortuna
+                </span>
+                <span className=" text-[.9rem] font-semibold text-violet-500 sm:text-base">
+                  Oct 2020 - Feb 2022 (1 year 5 months)
+                </span>
+                <p className=" text-[.9rem] text-justify break-words text-gray-500">
+                  - Conduct comprehensive quality testing on Applications (POS, Building Management, Smart
+                  Resto, Package Tracking, and Company Profile), ensuring thorough evaluation of functionality and
+                  performance.<br />
+                  - Develop and execute detailed test cases and scripts to cover various scenarios and ensure
+                  comprehensive quality assessment.<br />
+                  - Lead Application testing and validate user expectations.<br />
+                  - Systematically identify and document bugs in the Applications, inconsistencies, and deviations from
+                  quality standards.<br />
+                  - Collaborate with development, design, and product teams to provide clear explanations of identified
+                  issues.<br />
+                  - Maintain accurate and organized documentation of test results, methodologies, and bug reports.<br />
+                  - Regularly perform regression testing to ensure software updates maintain existing functionality.
+                </p>
+              </div>
+            </div>
+          </fieldset>
+        </div>
+
+        <div className="right flex-1 flex items-center justify-center">
+          <fieldset
+            data-aos="zoom-in"
+            className=" w-[80%] p-5 py-12 sm:py-8 sm:w-full sm:p-2"
+          >
+            <legend className=" w-auto ml-[50%] translate-x-[-50%] border-2 border-gray-200 rounded-3xl py-1 px-8 font-semibold text-xl text-violet-500">
+              Education
+            </legend>
+            <div className=" relative">
+              {/* design */}
+              <div className="design flex absolute left-[-150px] top-1/2 items-center rotate-[90deg] sm:left-[-160px] ">
+                <div className="c1 w-[12px] h-[12px] rounded-full bg-white border-2 border-violet-500"></div>
+                <div className="line w-[230px] bg-gray-300 h-[2px] sm:w-[250px]"></div>
+                <div className="c2 w-[12px] h-[12px] rounded-full bg-white border-2 border-violet-500"></div>
+              </div>
+              {/* design */}
+              <div className=" flex flex-col gap-1 border-2 border-violet-400 shadow-[0px_0px_16px_1px_rgba(0,0,0,0.1)] p-3 rounded-lg">
+                <h1 className="text-[1.4rem] font-semibold sm:text-xl">
+                  Vocational High School
+                </h1>
+                <span className=" text-[.9rem] font-semibold text-gray-500 sm:text-base">
+                  SMKN 2 Kab.Tangerang
+                </span>
+                <span className=" text-[.9rem] font-semibold text-violet-500 sm:text-base">
+                  2015 - 2018
+                </span>
+                <p className=" text-[.9rem] text-justify text-gray-500">
+                    Computer Systems Networking and Telecommunications
+                </p>
+              </div>
+            </div>
+          </fieldset>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Resume;
